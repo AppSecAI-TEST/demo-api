33b1561b39a4fd8049a048f9026f7c5219393619
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-08-10 20:09:19 +0800

    更新代码

--- src/main/java/com/company/project/api/demo/DemoApi.java
+++ src/main/java/com/company/project/api/demo/DemoApi.java
@@ -1,9 +1,8 @@
 package com.company.project.api.demo;
 
+import com.company.project.support.Action;
+import com.company.project.support.Api;
 import com.company.project.support.action.ActionExecutor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,18 +11,15 @@ import java.util.Map;
 /**
  * Created by wangzhj on 2017/8/10.
  */
-@Controller
-@RequestMapping("/demo")
+@Api("/demo")
 public class DemoApi {
 
-    @RequestMapping("/say_hi")
-    @ResponseBody
+    @Action("/say_hi")
     public Map<String, Object> say_hi(HttpServletRequest request, HttpServletResponse response) {
         return ActionExecutor.execute(request, response, Action_say_hi.class);
     }
 
-    @RequestMapping("/say_bye")
-    @ResponseBody
+    @Action("/say_bye")
     public Map<String, Object> say_bye(HttpServletRequest request, HttpServletResponse response) {
         return ActionExecutor.execute(request, response, Action_say_bye.class);
     }
